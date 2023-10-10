87b23a97f261447c22139ecbad537db772a3f6d6
Jamacio <jamacio.rocha@danone.com>
2023-10-10 11:54:56 -0300

    add type image accept to upload

--- certificates/index.php
+++ certificates/index.php
@@ -27,20 +27,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-
-
-  // Check if image file is a actual image or fake image
-  if (isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if ($check !== false) {
-      echo "File is an image - " . $check["mime"] . ".";
-      $uploadOk = 1;
-    } else {
-      echo "File is not an image.";
-      $uploadOk = 0;
-    }
-  }
-
   // Check if file already exists
   if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
