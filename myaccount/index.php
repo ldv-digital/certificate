a0df9a47014c3043052079d44a7107a86b95b57e
mauricioc08 <mauricio-cassiano@hotmail.com>
2023-10-25 11:35:33 -0300

    add Adsense

--- myaccount/index.php
+++ myaccount/index.php
@@ -55,6 +55,10 @@ require_once "../config.php";
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <link rel="stylesheet" href="../css/main.css">
   <link rel="shortcut icon" href="../img/favicon-16x16.png" type="image/x-icon">
+  <!-- Adsense -->
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6667247105321030"
+  crossorigin="anonymous"></script>
+  <!-- Adsense -->
 </head>
 
 <body>
